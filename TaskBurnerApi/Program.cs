b5f79ebe9793a1618a4cb0bd0b3b6e7e4a6ee8f7
Logan <lodev205@gmail.com>
2025-03-27 17:43:40 -0500

    :loud_sound: Enhance logging and update project structure
    - Implement logging capabilities, including console and debug logging for development.
    - Add logging for root and issue endpoints to track requests and responses.
    - Modifiy the root endpoint response to include a version number.
    - Include a directory for future service implementations

--- TaskBurnerApi/Program.cs
+++ TaskBurnerApi/Program.cs
@@ -1,21 +1,42 @@
 using TaskBurnerAPI.Data;
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Logging.AddDebug();
+}
+
+builder.Logging.AddEventSourceLogger();
+
 var db = new TaskBurnerDB();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "TaskBurner API running");
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation($"Starting TaskBurner API\n\t{DateTime.UtcNow}");
+app.MapGet("/", () =>
+{
+    logger.LogInformation($"Root endpoint called:\n\t{DateTime.UtcNow}");
+    return "TaskBurner API v0.0.x";
+});
 
 app.MapGet("/issues", () =>
 {
+    logger.LogInformation("Fetching all issues");
     return Results.Ok(db.Issues);
 });
 
 app.MapGet("/issues/{id}", (int id) =>
 {
+    logger.LogInformation("Fetching issue with ID {Id}", id);
     var issue = db.Issues.SingleOrDefault(i => i.Id == id);
     if (issue is null)
     {
+        logger.LogWarning("Issue with ID {Id} not found", id);
         return Results.NotFound();
     }
     return Results.Ok(issue);
@@ -23,7 +44,6 @@ app.MapGet("/issues/{id}", (int id) =>
 
 app.Run();
 
-public partial class Program()
+public partial class Program
 {
-
 }
