4f83765983d6527d7ea17fa5e9e550e6c14b9895
Logan <lodev205@gmail.com>
2025-03-28 14:39:41 -0500

    :sparkles: Add initial logging service integration

--- TaskBurnerApi/Program.cs
+++ TaskBurnerApi/Program.cs
@@ -3,6 +3,10 @@ using TaskBurnerAPI.Enums;
 using TaskBurnerAPI.Models;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient("LoggingService", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5142/api/logs/");
+});
 // TODO: Implement HttpClient connection to LogService 
 
 // TODO: Implement API Documentation (i.e. Swagger, OpenApi)
@@ -27,11 +31,32 @@ var app = builder.Build();
 
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
-logger.LogInformation($"Starting TaskBurner API\n\t{DateTime.UtcNow}");
+logger.LogInformation("Starting TaskBurner API\n\t{DateTime.UtcNow}", DateTime.UtcNow);
 
-app.MapGet("/", () =>
+async Task SendLogToLoggingService(LogLevel logLevel, string message, IHttpClientFactory httpClientFactory)
 {
-    logger.LogInformation($"Root endpoint called:\n\t{DateTime.UtcNow}");
+    var client = httpClientFactory.CreateClient("LoggingService");
+    var logMessage = new
+    {
+        LogLevel = logLevel,
+        Message = message
+    };
+
+    try
+    {
+        await client.PostAsJsonAsync("log", logMessage);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "Failed to send log to LoggingService");
+    }
+}
+
+app.MapGet("/", async (IHttpClientFactory httpClientFactory) =>
+{
+    var message = $"TaskBurnerAPI Root endpoint called:\n\t{DateTime.UtcNow}";
+    logger.LogInformation(message);
+    await SendLogToLoggingService(LogLevel.Information, message, httpClientFactory);
     return "TaskBurner API v0.0.x";
 });
 
@@ -56,9 +81,9 @@ app.MapGet("/issues/{id}", (int id) =>
 app.MapPost("/issues", (Issue issue) =>
 {
     logger.LogInformation("Creating new issue");
-    
+
     if (issue.Title == string.Empty)
-    { 
+    {
         logger.LogWarning("Invalid request: Title empty");
         return Results.BadRequest();
     }
@@ -67,7 +92,7 @@ app.MapPost("/issues", (Issue issue) =>
     {
         logger.LogWarning("Invalid request: Body empty");
         return Results.BadRequest();
-    }   
+    }
 
     issue.Id = db.Issues.Max(i => i.Id) + 1;
     db.Issues.Add(issue);
@@ -76,7 +101,7 @@ app.MapPost("/issues", (Issue issue) =>
 
 app.MapPut("/issues/{id}/status", (int id, Issue item) =>
 {
-    if(id <= 0)
+    if (id <= 0)
     {
         logger.LogWarning("ID cannot be 0 or negative");
         return Results.BadRequest();
